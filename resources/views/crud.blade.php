69279938aa24dbd1569106132d8888d224b33e95
willywin99 <willywinsianturi@gmail.com>
2020-12-22 06:16:02 +0700

    add editStatus to change add or update button

--- resources/views/crud.blade.php
+++ resources/views/crud.blade.php
@@ -5,19 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    {{-- <style>
+        .edit-status{
+
+        }
+    </style> --}}
 </head>
 <body>
 
     <div id="app">
         <h3>CRUD</h3>
-        <input type="text" >
-        <button type="button">Add</button>
+        <input type="text" v-model="newCrud" >
+        <button type="button" @click.prevent="addCrud" v-show="!editStatus">Add</button>
+        <button type="button" @click.prevent="updateCrud" v-show="editStatus">Update</button>
         <ul>
             <li v-for="(crud, index) in cruds">
                 <span> @{{ crud.nama }} </span>
-                <button type="button"> Edit </button>
+                <button type="button" v-on:click="editCrud(index)"> Edit </button>
                 ||
-                <button type="button"> Delete </button>
+                <button type="button" v-on:click="removeCrud(index)"> Delete </button>
             </li>
         </ul>
     </div>
@@ -30,11 +36,31 @@
             data: {
                 newCrud: '',
                 cruds: [
-                    { id: 1,    nama: 'Muhammad Iqbal Mubarok' },
-                    { id: 2,    nama: 'Ruby Purwanti' },
-                    { id: 3,    nama: 'Faqih Muhammad' },
+                    { id: 1,    nama: 'Muhammad Iqbal Mubarok', },
+                    { id: 2,    nama: 'Ruby Purwanti', },
+                    { id: 3,    nama: 'Faqih Muhammad', },
                 ],
-            }
+                editStatus: false,
+            },
+            methods: {
+                addCrud: function() {
+                    let textInput = this.newCrud.trim();
+                    if(textInput) {
+                        this.cruds.unshift(
+                            { nama: textInput }
+                        )
+                    }
+                    // console.log(textInput);
+                },
+                removeCrud: function(index) {
+                    this.cruds.splice(index, 1);
+                },
+                editCrud: function(index) {
+                    this.editStatus = !this.editStatus;
+                    // console.log(this.editStatus);
+                }
+            },
+
         })
     </script>
 
